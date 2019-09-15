e7d46ea6ab92ceabf027aed61a8b1052ea47a5b0
Thomas Ardal <thomasardal@gmail.com>
2019-09-15 14:06:01 +0200

    Pusing server variables, cookies, form and query string to log context as well

--- src/Elmah.Io.AspNetCore.Serilog/ElmahIoSerilogMiddleware.cs
+++ src/Elmah.Io.AspNetCore.Serilog/ElmahIoSerilogMiddleware.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Serilog.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Elmah.Io.AspNetCore.Serilog
@@ -19,7 +22,40 @@ namespace Elmah.Io.AspNetCore.Serilog
             using (LogContext.PushProperty("method", context.Request?.Method))
             using (LogContext.PushProperty("statuscode", context.Response.StatusCode))
             using (LogContext.PushProperty("user", context.User?.Identity?.Name))
+            using (LogContext.PushProperty("servervariables", ServerVariables(context)))
+            using (LogContext.PushProperty("cookies", Cookies(context)))
+            using (LogContext.PushProperty("form", Form(context)))
+            using (LogContext.PushProperty("querystring", QueryString(context)))
                 await _next.Invoke(context);
+        }
+
+        private Dictionary<string, string> QueryString(HttpContext context)
+        {
+            return context.Request?.Query?.Keys.ToDictionary(k => k, k => context.Request.Query[k].ToString());
+        }
+
+        private Dictionary<string, string> Form(HttpContext context)
+        {
+            try
+            {
+                return context.Request?.Form?.Keys.ToDictionary(k => k, k => context.Request.Form[k].ToString());
+            }
+            catch (InvalidOperationException)
+            {
+                // Request not a form POST or similar
+            }
+
+            return new Dictionary<string, string>();
+        }
+
+        private Dictionary<string, string> Cookies(HttpContext context)
+        {
+            return context.Request?.Cookies?.Keys.ToDictionary(k => k, k => context.Request.Cookies[k].ToString());
+        }
+
+        private Dictionary<string, string> ServerVariables(HttpContext context)
+        {
+            return context.Request?.Headers?.Keys.ToDictionary(k => k, k => context.Request.Headers[k].ToString());
         }
     }
 }
