66d21734b4c89f2234a8c54684d34bd1dce10685
Thomas Ardal <thomasardal@gmail.com>
2020-12-12 12:15:44 +0100

    Documentation on use method

--- src/Elmah.Io.AspNetCore.Serilog/ElmahIoAspNetCoreSerilogExtensions.cs
+++ src/Elmah.Io.AspNetCore.Serilog/ElmahIoAspNetCoreSerilogExtensions.cs
@@ -4,6 +4,11 @@ namespace Microsoft.AspNetCore.Builder
 {
     public static class ElmahIoAspNetCoreSerilogExtensions
     {
+        /// <summary>
+        /// Install the elmah.io middleware for Serilog. The middleware will decorate Serilogs LogContext
+        /// with HTTP contextual information that the elmah.io sink will pick up when logging messages.
+        /// To use this middleware make sure to also call the Enrich.FromLogContext() method.
+        /// </summary>
         public static IApplicationBuilder UseElmahIoSerilog(this IApplicationBuilder app)
         {
             return app.UseMiddleware<ElmahIoSerilogMiddleware>();
